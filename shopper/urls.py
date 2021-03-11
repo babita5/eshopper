db6f0c8eba48a2dc982970d4892e5df864bc0c54
babita5 <babitasingh324@gmail.com>
2021-03-11 21:10:08 +0545

    render index.html

--- shopper/urls.py
+++ shopper/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
+from .views import *
+app_name='shopper'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('shopper.urls')),
+    path('',HomeView.as_view(),name='home'),
+
 ]
--- shopper/views.py
+++ shopper/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .views import *
+from django.views.generic.base import View
 # Create your views here.
+class BaseView(View):
+    views={}
+
+class HomeView(BaseView):
+    def get(self,request):
+        return render(request,'index.html')
